9d19bda14df72d43b9c3996bee583ba57f776ca6
Julia (Goll) Amelchanka <48388987+JuliaTskuba@users.noreply.github.com>
2021-01-21 14:42:51 -0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        // singe char only vs string (many)
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        char myUnicodeCharJ = '\u00A9';
+        //System.out.println(myChar);
+        //System.out.println(myUnicodeChar);
+        System.out.println(myUnicodeCharJ);
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+
+        // System.out.println("Is she over twenty one? Answer: " + isCustomerOverTwentyOne);
+
+        
+
+    }
+}
