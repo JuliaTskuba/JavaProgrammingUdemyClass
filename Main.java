a927398c712a51cbd0846391e44cc9256d99fc21
Julia (Goll) Amelchanka <48388987+JuliaTskuba@users.noreply.github.com>
2021-01-21 14:58:50 -0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int[] list ={1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int i : list)
+            System.out.print(i + ", ");
+    }
+}
