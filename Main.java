8be6bfdcc1a6da965e5df46e0e8dae6a0c20cc03
Julia (Goll) Amelchanka <48388987+JuliaTskuba@users.noreply.github.com>
2021-01-21 15:22:43 -0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package academy.learnprogramming;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Please enter a number: ");
+        abc x = new abc();
+        x.X();
+    }
+}
---
+++ abc.java
@@ -0,0 +1,14 @@
+package academy.learnprogramming;
+
+import java.util.Scanner;
+
+public class abc {
+    public  int x = s.nextInt();;
+    public static Scanner s = new Scanner(System.in);
+
+    void X() {
+        for (int x = 1; x <= 12; x++) {
+            System.out.println(x + " times " + this.x + " equals " + this.x * x);
+        }
+    }
+}
